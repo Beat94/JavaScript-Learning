d2a5a0ca64120bc7f827f321b22461e513289379
Beat94 <36741096+Beat94@users.noreply.github.com>
2018-06-20 16:32:31 +0200

    Delete calculation.js

--- JavaScript-Learning-master/JavaScript-Learning-master/scripts/calculation.js
+++
@@ -1,7 +0,0 @@
-addition(){
-
-  num1 = parseInt(document.formu.adnum1.value);
-  num2 = parseInt(document.formu.adnum2.value);
-  document.getElementById("output").innerHTML = (num1 + num2).toString();
-
-}
